972ea8ec4e69a3e3275be64552f319198468200a
zhangbyby <zhangbyby@outlook.com>
2023-03-10 10:01:37 +0800

    添加新需求

--- src/main/java/com/zhangbyby/bfc/component/list/render/JListPsiFieldCellRenderer.java
+++ src/main/java/com/zhangbyby/bfc/component/list/render/JListPsiFieldCellRenderer.java
@@ -2,13 +2,15 @@ package com.zhangbyby.bfc.component.list.render;
 
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiModifier;
+import com.intellij.psi.PsiModifierList;
 import com.intellij.ui.JBColor;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.Optional;
 
-import static com.intellij.icons.AllIcons.Nodes.Field;
+import static com.intellij.icons.AllIcons.Nodes.*;
 
 /**
  * JList组件 元素渲染器
@@ -24,10 +26,26 @@ public class JListPsiFieldCellRenderer implements ListCellRenderer<PsiField> {
 
     @Override
     public Component getListCellRendererComponent(JList list, PsiField value, int index, boolean isSelected, boolean cellHasFocus) {
-        JLabel jLabel = new JLabel();
-        jLabel.setText(value.getName() + ": " + value.getType().getPresentableText());
-        jLabel.setIcon(Field);
-        jLabel.setToolTipText(Optional.ofNullable(value.getContainingClass())
+        JLabel container = new JLabel();
+        container.setLayout(new BoxLayout(container, BoxLayout.X_AXIS));
+
+        PsiModifierList modifierList = value.getModifierList();
+        if (modifierList != null) {
+            if (modifierList.hasExplicitModifier(PsiModifier.STATIC)) {
+                JLabel staticMarkLabel = new JLabel();
+                staticMarkLabel.setIcon(StaticMark);
+                container.add(staticMarkLabel);
+            }
+            if (modifierList.hasExplicitModifier(PsiModifier.FINAL)) {
+                JLabel finalMarkLabel = new JLabel();
+                finalMarkLabel.setIcon(FinalMark);
+                container.add(finalMarkLabel);
+            }
+        }
+
+        container.setText(value.getName() + ": " + value.getType().getPresentableText());
+        container.setIcon(Field);
+        container.setToolTipText(Optional.ofNullable(value.getContainingClass())
                 .map(PsiClass::getQualifiedName).orElse("") + "#" + value.getName());
 
         int i = 0;
@@ -37,9 +55,9 @@ public class JListPsiFieldCellRenderer implements ListCellRenderer<PsiField> {
             }
         }
         if (i == anotherList.getModel().getSize()) {
-            jLabel.setForeground(JBColor.RED);
+            container.setForeground(JBColor.RED);
         }
 
-        return jLabel;
+        return container;
     }
 }
