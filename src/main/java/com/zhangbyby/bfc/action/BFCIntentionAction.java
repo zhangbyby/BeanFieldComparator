1f23ece4f5fcf58a7f8b7f31b04ce488dfa6140b
zhangbyby <zhangbyby@outlook.com>
2023-03-23 10:41:40 +0800

    add some logs

--- src/main/java/com/zhangbyby/bfc/action/BFCIntentionAction.java
+++ src/main/java/com/zhangbyby/bfc/action/BFCIntentionAction.java
@@ -3,7 +3,9 @@ package com.zhangbyby.bfc.action;
 import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction;
 import com.intellij.codeInspection.util.IntentionFamilyName;
 import com.intellij.codeInspection.util.IntentionName;
+import com.intellij.idea.LoggerFactory;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
@@ -14,6 +16,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * 上下文操作入口
@@ -21,6 +24,8 @@ import java.util.List;
  * @author zhangbyby
  */
 public class BFCIntentionAction extends PsiElementBaseIntentionAction {
+
+    private static final Logger logger = new LoggerFactory().getLoggerInstance(BFCIntentionAction.class.getSimpleName());
 
     public BFCIntentionAction() {
     }
@@ -39,6 +44,9 @@ public class BFCIntentionAction extends PsiElementBaseIntentionAction {
         String sourceClassName = isApache ? classNames.get(1) : classNames.get(0);
         String targetClassName = isApache ? classNames.get(0) : classNames.get(1);
 
+        logger.info("sourceClassName: "+ sourceClassName);
+        logger.info("targetClassName: "+ targetClassName);
+
         BFCDialogWrapper.sourceClassName = sourceClassName;
         BFCDialogWrapper.targetClassName = targetClassName;
         ApplicationManager.getApplication().invokeLater(() -> {
@@ -53,24 +61,31 @@ public class BFCIntentionAction extends PsiElementBaseIntentionAction {
     public boolean isAvailable(@NotNull Project project, Editor editor, @NotNull PsiElement element) {
         PsiElement parent = element.getParent();
         if (!(parent instanceof PsiReferenceExpression)) {
+            logger.info("parent element is not instanceof PsiReferenceExpression: " + parent.getClass());
             return false;
         }
         PsiElement context = parent.getContext();
         if (!(context instanceof PsiMethodCallExpression)) {
+            logger.info("parent element context is not instanceof PsiMethodCallExpression: " + Optional.ofNullable(context).map(Object::getClass).map(Class::getCanonicalName).orElse(null));
             return false;
         }
-        if (!context.getText().contains(".copyProperties")) {
+        if (!context.getText().contains("copyProperties")) {
+            logger.info("context is not contains 'copyProperties': " + context.getText());
             return false;
         }
         PsiMethod psiMethod = ((PsiMethodCallExpression) context).resolveMethod();
         if (psiMethod == null) {
+            logger.info("context resolve method is null");
             return false;
         }
+        logger.info("method name: " + psiMethod.getName());
 
         PsiClass methodClass = (PsiClass) psiMethod.getContext();
         if (methodClass == null || methodClass.getQualifiedName() == null) {
+            logger.info("method PsiClass is null or PsiClass qualifiedName is null");
             return false;
         }
+        logger.info("class qualifiedName: " + methodClass.getQualifiedName());
 
         return readArgumentsQualifiedClassName((PsiMethodCallExpression) context) != null;
     }
@@ -92,6 +107,7 @@ public class BFCIntentionAction extends PsiElementBaseIntentionAction {
             }
             classNames.add(psiClass.getQualifiedName());
         }
+        logger.info("readArgumentsQualifiedClassName result: " + classNames);
         return classNames;
     }
 
